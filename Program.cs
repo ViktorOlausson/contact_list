4b8bcd0bf886caab553c3faac96dfa0808edd618
ViktorOlausson <02viktorolausson@gmail.com>
2023-10-30 16:23:39 +0100

    Bröt ut static-metoder för ReadContactListFromFile och WriteContactListToFile

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@
             print_help();
             do
             {
-                Console.Write($"> ");
+                
                 commandLine = Input($"> ").Split(' ');
                 
                 if (commandLine[0] == "quit")
@@ -29,77 +29,30 @@
                     if (commandLine.Length < 2)
                     {
                         lastFileName = "address.txt";
-                        using (StreamReader infile = new StreamReader(lastFileName)) //FIXME: om filen inte finns
-                        {
+                        //FIXME: om filen inte finns
+
                             string line;
-                            while ((line = infile.ReadLine()) != null)
-                            {
-                                Console.WriteLine(line);
-                                string[] attrs = line.Split('|');
-                                Person p = new Person();
-                                p.persname = attrs[0];
-                                p.surname = attrs[1];
-                                string[] phones = attrs[2].Split(';');
-                                p.phone = phones[0];
-                                string[] addresses = attrs[3].Split(';');
-                                p.address = addresses[0];
-                                for (int ix = 0; ix < contactList.Length; ix++)
-                                {
-                                    if (contactList[ix] == null)
-                                    {
-                                        contactList[ix] = p;
-                                        break;
-                                    }
-                                }
-                            }
-                        }
+                            ReadContactListFromFile(lastFileName);
+                        
                     }
                     else
                     {
                         lastFileName = commandLine[1];
-                        using (StreamReader infile = new StreamReader(lastFileName))
-                        {
-                            string line;
-                            while ((line = infile.ReadLine()) != null)
-                            {
-                                Console.WriteLine(line);
-                                string[] attrs = line.Split('|');
-                                Person p = new Person();
-                                p.persname = attrs[0];
-                                p.surname = attrs[1];
-                                string[] phones = attrs[2].Split(';');
-                                p.phone = phones[0];
-                                string[] addresses = attrs[3].Split(';');
-                                p.address = addresses[0];
-                                for (int ix = 0; ix < contactList.Length; ix++)
-                                {
-                                    if (contactList[ix] == null)
-                                    {
-                                        contactList[ix] = p;
-                                        break;
-                                    }
-                                }
-                            }
-                        }
+                        ReadContactListFromFile(lastFileName);
                     }
                 }
                 else if (commandLine[0] == "save")
                 {
                     if (commandLine.Length < 2)
                     {
-                        using (StreamWriter outfile = new StreamWriter(lastFileName))
-                        {
-                            foreach (Person p in contactList)
-                            {
-                                if (p != null)
-                                    outfile.WriteLine($"{p.persname};{p.surname};{p.phone};{p.address};{p.birthdate}");
-                            }
-                        }
+                        WriteContactListToFile(lastFileName);
                     }
                     else
                     {
                         // NYI: save file
                         Console.WriteLine("Not yet implemented: save /file/");
+                        lastFileName = commandLine[1];
+                        WriteContactListToFile(lastFileName);
                     }
                 }
                 else if (commandLine[0] == "new")
@@ -130,6 +83,47 @@
                 }
             } while (commandLine[0] != "quit");
         }
+
+        private static void WriteContactListToFile(string lastFileName)
+        {
+            using (StreamWriter outfile = new StreamWriter(lastFileName))
+            {
+                foreach (Person p in contactList)
+                {
+                    if (p != null)
+                        outfile.WriteLine($"{p.persname};{p.surname};{p.phone};{p.address};{p.birthdate}");
+                }
+            }
+        }
+
+        private static void ReadContactListFromFile(string lastFileName)
+        {
+            using (StreamReader infile = new StreamReader(lastFileName))
+            {
+                string line;
+                while ((line = infile.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    string[] attrs = line.Split('|');
+                    Person p = new Person();
+                    p.persname = attrs[0];
+                    p.surname = attrs[1];
+                    string[] phones = attrs[2].Split(';');
+                    p.phone = phones[0];
+                    string[] addresses = attrs[3].Split(';');
+                    p.address = addresses[0];
+                    for (int ix = 0; ix < contactList.Length; ix++)
+                    {
+                        if (contactList[ix] == null)
+                        {
+                            contactList[ix] = p;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
         static string Input(string prompt)
         {
             Console.Write(prompt);
